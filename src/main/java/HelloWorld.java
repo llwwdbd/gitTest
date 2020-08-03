b091ac39c6cdf566fdeb7843fc608618ddb29901
llwwdbd <289367860@qq.com>
2020-08-03 14:48:20 +0800

    又加了一行

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main() {
         System.out.println("hello world");
         System.out.println("bye");
+        System.out.println("github");
     }
 }
